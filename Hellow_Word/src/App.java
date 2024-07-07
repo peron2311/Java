0a131c4347234ee8e7994e56fb6d5a9ce9a4f0ab
FERNANDO PERON <fernando@szsolucoes.com.br>
2024-07-07 16:30:51 -0300

    Teste de Alteração

--- Hellow_Word/src/App.java
+++ Hellow_Word/src/App.java
@@ -1,15 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int a = 1;
-        int b=2;
+        String frase = "Hello World";
 
-        int calcular = calcular(a,b);
-
-        System.out.println(calcular);
+        String show = show(frase);
+        System.out.println(show);
     }
     
-    public static int calcular (int a, int b) {
-        return a+b+5;
+    public static String show (String frase) {
+        return  frase;
     }
 
 }
