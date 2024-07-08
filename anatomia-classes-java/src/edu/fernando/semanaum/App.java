6d4f3838d2417a101ea3677ba6f167a541fedbc8
FERNANDO PERON <fernando@szsolucoes.com.br>
2024-07-07 23:22:45 -0300

    Correção texto

--- anatomia-classes-java/src/edu/fernando/semanaum/App.java
+++ anatomia-classes-java/src/edu/fernando/semanaum/App.java
@@ -1,6 +1,6 @@
 package edu.fernando.semanaum;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! ");
+        System.out.println("Hello, World!");
     }
 }
