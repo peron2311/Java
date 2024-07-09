6d36176cb338d2a337c8ae85b740df5518b4ed19
FERNANDO PERON <fernando@szsolucoes.com.br>
2024-07-08 22:31:30 -0300

    Aula Contastes

--- anatomia-classes-java/src/edu/fernando/semanaum/App.java
+++ anatomia-classes-java/src/edu/fernando/semanaum/App.java
@@ -1,6 +1,21 @@
 package edu.fernando.semanaum;
 public class App {
     public static void main(String[] args) throws Exception {
+
+
         System.out.println("Hello, World!");
+
+        System.out.println(Teste());
     }
+
+    public static int Teste(){
+        
+        final int TESTE = 10; // constante
+        
+        return TESTE;
+    }
+
+
+    
+    
 }
