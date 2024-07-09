f3ad005968fd31d2807451c0f002eb782c97f8c1
FERNANDO PERON <fernando@szsolucoes.com.br>
2024-07-09 08:48:38 -0300

    Aprendendo Métodos

---
+++ Metodos/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println(somar(1,6));
+    }
+
+    public static  int somar (int a, int b){
+         return a+b;
+    }
+}
